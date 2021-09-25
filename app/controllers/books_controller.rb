b973c12fe4640baff19f50e1458998263f8b5682
Aleksandr Bogomolov <boganet2000@gmail.com>
2021-09-26 01:06:53 +0300

    added code to realize CRUD for Book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
 class BooksController < ApplicationController
+  include Response
+  before_action :set_book, only: [:show, :update, :destroy]
 
   def index
     @books = Book.all
-    render json: @books
+    json_response(@books)
+  end
+
+  def create
+    @book = Book.create!(book_params)
+    json_response(@book, :created)
+  end
+
+  def update
+    @book.update(book_params)
+    head :no_content
   end
 
   def show
@@ -11,5 +23,19 @@ class BooksController < ApplicationController
     if stale?(last_modified: @book.updated_at)
       render json: @book
     end
+  end
+
+  def destroy
+    @book.destroy
+    head :no_content
+  end
+
+  def book_params
+    # whitelist params
+    params.permit(:title, :author, :description)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
