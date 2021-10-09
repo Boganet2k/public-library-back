1549eca7b4fbc48edd6565752421eeb0c1658cdd
Aleksandr Bogomolov <boganet2000@gmail.com>
2021-10-09 18:58:26 +0300

    app name for domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'public-library-gradeus-reactjs.heroku.com',
+    :domain         => 'public-library-gradeus.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
