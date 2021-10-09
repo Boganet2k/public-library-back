b5ea4d22f563a25a588c711ebc4649485e242287
Aleksandr Bogomolov <boganet2000@gmail.com>
2021-10-10 01:48:07 +0300

    added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,25 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    @url  = 'https://public-library-gradeus-reactjs.herokuapp.com'
+    @url = ENV['APP_URL']
     mail(to: @user.email, subject: 'Welcome to Public library')
+  end
+
+  def reservation_confirmed
+    @reservation = params[:reservation]
+    @reservation_expired_at = params[:reservation_expired_at]
+    @url = ENV['APP_URL']
+
+    mail(to: @reservation.user.email, subject: 'Reservation in Public library confirmed')
+  end
+
+  def reservation_expired
+
+    @reservation = params[:reservation]
+    @reservation_expired_at = params[:reservation_expired_at]
+    @url = ENV['APP_URL']
+
+    mail(to: @reservation.user.email, subject: 'Reservation in Public library expired')
   end
 
 end
